29e91a8e45b5dbb88d578de089b14e29711bd901
tomass09 <tomhste@gmail.com>
2016-09-14 11:05:14 +0000

    allur kodinn

--- verkefni1/src/tftpd.c
+++ verkefni1/src/tftpd.c
@@ -1,17 +1,34 @@
+#include <assert.h>
+#include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <sys/time.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 /*
-firs input: portnumber.
-second input: name of dir.
-will be done on server. 
+The pac
 */
+/*            
+    pac = top  
+           2byte|    2byte|max512byte | 1byte      
+              op|errorcode|errormsg   | 0
+    */
+
+struct pacInfo
+{
+    /*The data we need to*/
+    int sockfd;
+    char message[516];
+    char payload[512];
+    int sizeOfData;
+    struct sockaddr server, client;
+    char options[2];
+};
 
 //values for time
 struct timeval timeStart;
@@ -20,134 +37,195 @@ extern int timeval;
 int timepassed;
 int maxtime = 500;
 
-struct pacInfo
-{
-    /*The data we need to*/
-    int sockfd;
-    char message[516];
-    struct sockaddr client;
-};
-/*
-static void timererror (time)
-{
-// hér þarf að útfæra time error klasann ss. ef við förum fyrir maxtime 
-}
 
-// Time of start of trans
-static void startTime()
+void sendErr( int errorType,struct pacInfo pac)
 {
-	(void)gettimeofday(&timeStart, NULL);
-}
-
-// Time of end tran
-static void stopTime()
-{
-	(void)gettimeofday(&timeStop, NULL);
-}
-*/
-
-
-void sendErr( int errorType,struct pacInfo packet)
-{
-    /*            
-    packet = 
-           2byte|    2byte|max512byte | 1byte      
-              op|errorcode|errormsg   | 0
-    */
-
-    memset(&packet.message[0],0,sizeof(packet.message)); // clears array
+    
+    memset(&pac.message[0],0,sizeof(pac.message)); // clears array
+    //I foundthis union in piazza. 
     union {unsigned short opCode; char arr[2];} converted; //makes tuple for convertions
     converted.opCode = htons(5); // networkbyte order
-    packet.message[0] = converted.arr[0];
-    packet.message[1] = converted.arr[1];
+    pac.message[0] = converted.arr[0];
+    pac.message[1] = converted.arr[1];
 
     converted.opCode = htons(errorType); // networkbyte order
-    packet.message[2] = converted.arr[0];
-    packet.message[3] = converted.arr[1];
-    //TODO need to check with error msg with char* errorMsg
-    //memcpy(&packet.message[4],&errorMsg,sizeof(errorMsg));
+    pac.message[2] = converted.arr[0];
+    pac.message[3] = converted.arr[1];
+    //TODO need to check with error msg with char* errorMsg need to reconsider this sento msg
+    //memcpy(&pac.message[4],&errorMsg,sizeof(errorMsg));
+
+    /*  
+    n = Specifies the size of the message in bytes.
+    len = Specifies the length of the sockaddr structure pointed to by the dest_addr argument.
+    4 = the size of hedder i cant se a reson why i would not just have that hardcoded. 
+    */
+    sendto(pac.sockfd, pac.message, (size_t) sizeof(4), 0,(struct sockaddr *) &pac.client, sizeof(pac.client));  
+}
 
 
-        /*  
-        n = Specifies the size of the message in bytes.
-        len = Specifies the length of the sockaddr structure pointed to by the dest_addr argument.
-        */
-    sendto(packet.sockfd, packet.message, (size_t) sizeof(4), 0,
-           (struct sockaddr *) &packet.client, sizeof(packet.client));  
+// LETS SEND SUM DATA !
+// OMG HOW DO I SEND DATA ?
+/*
+ what do we need in the void fuction ? 
+    1.1 we need the file name -check
+    1.2 we need the structure for the packet - check
+    1.3 we need the number of the packet - l8er
+    1.3 send the pac -CHECK! 
+*/
+
+void sendPac ( unsigned short pacNum, char srcFile[], struct pacInfo pac)
+{
+   
+    FILE *fileTosend = fopen(srcFile, pac.options);
+    memset(&pac.message[0],0,sizeof(pac.message));// clears array message
+    memset(&pac.payload[0],0,sizeof(pac.payload));//clears array payload
+    if ( fileTosend == NULL) // this if is off need to check if there is time
+    {
+        sendErr(1, pac);
+        return; 
+    }
+    if ( fileTosend != NULL)
+    {
+        if (pacNum > 1)
+        {
+            fseek (fileTosend, (pacNum-1)*512, SEEK_SET );
+        }
+    pac.sizeOfData = fread(&pac.payload, 1, 512,fileTosend);
+    fclose(fileTosend);
+    }
+
+    strcpy(&pac.message[4], pac.payload);
+    union {unsigned short opCode; char arr[2];} converted;
+    pac.message[0] = converted.arr[0];
+    pac.message[1] = converted.arr[1];
+
+    converted.opCode = htons(pacNum); // networkbyte order
+    pac.message[2] = converted.arr[0];
+    pac.message[3] = converted.arr[1];
+    sendto(pac.sockfd, pac.message, (size_t) sizeof(516), 0,(struct sockaddr *) &pac.client,(socklen_t) sizeof(pac.client));
+    
+}
+/*
+ well now we can send stuff but just one time lets devide the data.
+ I think I will only need a counter for the data but lets see 
+*/
+// data management
+unsigned short next(char message[])
+{    // this is straight from piazza
+    union {unsigned short opCode; char arr[2];} converted;
+    converted.arr[0] = message[2];
+    converted.arr[1] = message[1];
+    converted.opCode = ntohs(converted.opCode);
+    return converted.opCode + 1;
 }
 
 int main(int argc, char *argv[])
 {
-	//fprintf(stdout, "argc is%d", argc);
-	if (argc  != 3 )
-	{
-	   perror("Please enter ./tftpd.c portnumer directory");
-	   exit(1);
-	}
 
-    struct pacInfo packet;
-    struct sockaddr_in server;
+    /*
+    firs input: portnumber.
+    second input: name of dir.
+    will be done on server. 
+    */
+    //fprintf(stdout, "argc is%d", argc);
+    if (argc  != 3 )
+    {
+       perror("Please enter ./tftpd.c portnumer directory");
+       exit(1);
+    }
+    if (chdir(argv[2]) != 0)// dir not found that client has asked for
+    {
+        perror("Directory is not found");
+        exit(1);
+    }
+    int port = atoi(argv[1]);
+    // new instans of pacInfo
+    struct pacInfo pac;
+    pac.options[0] = 'r';
+    pac.options[1] = ' ';
+    char namOfSrc = [30];
+
+    // is that all for ther server port or am i missin something
+
     /* Create and bind a UDP socket */
-    packet.sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
+    pac.sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&pac.server, 0, sizeof(pac.server));
+    pac.server.sin_family = AF_INET;
 
     /* Network functions need arguments in network byte order instead
      * of host byte order. The macros htonl, htons convert the
      * values.
      */
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(57596);
-    bind(packet.sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+    pac.server.sin_addr.s_addr = htonl(INADDR_ANY);
+    pac.server.sin_port = htons(port);
+    bind(pac.sockfd, (struct sockaddr *) &pac.server, (socklen_t) sizeof(pac.server));
 
     for (;;) {
-        /* Receive up to one byte less than declared, because it will
-         * be NUL-terminated later.
-         */
-        socklen_t len = (socklen_t) sizeof(packet.client);
-        ssize_t n = recvfrom(packet.sockfd, packet.message, sizeof(packet.message) - 1,
-                             0, (struct sockaddr *) &packet.client, &len);
+                // new instans of timeval
+                struct timval tv;
+                // set for scoket
+                fd_set rfds;
+                int returnValue;
 
-        packet.message[n] = '\0';
-        fprintf(stdout, "Received:\n%d\n", (int)packet.message[1]);
-        fflush(stdout);
-        int  number = (int)packet.message[1];
-        switch (number)
-        {
-            case 1: //Get
-                fprintf(stdout, "Recived %d",number);
-                // chekc if dir is accesable && send first packet
-                break;
-            case 2: //Put
-                fprintf(stdout, "Recived %d",number);
-                // Send ERROR packet
-                sendErr(2,packet);
-                break;
-            case 3://Data
-                fprintf(stdout, "Recived %d",number);
-                // Send Erro packet
-                sendErr(2,packet);
-                break;
-            case 4: //Ack
-                fprintf(stdout, "Recived %d",number);
-                // block number resived  && send next packet if not last
-                break;
-            case 5://Error
-                fprintf(stdout, "Recived %d",number);
-                sendErr(2,packet); // þurfum að ákveða hvaða error við viljum
-                // nokkuð viss um að við eigum ekki að gera neitt.
-                break;
-            default: 
-             sendErr(4,packet);
-            break;// client fault
+                //most of what is left in main is done in deamatimi.
+                FD_ZERO(&rfds);
+                FD_SET(pac.sockfd, &rfds);
 
-        }
-/*
-        sendto(packet.sockfd, packet.message, (size_t) n, 0,
-               (struct sockaddr *) &packet.client, len);
-               */
+                // Time management
+                tv.tv_sec = 10;
+                tv.tv_usec = 0;
+                returnValue = select(pac.sockfd + 1, &rfds, NULL, NULL, &tv);
+                if( returnValue == -1)
+                {
+                    perror("Oops something whent wrong fix");
+
+                }
+                else if (returnValue > 0)
+                {
+                    assert(FD_ISSET(pac.sockfd, &rfds)); // Right src of data found. 
+                    socklen_t len = (socklen_t) sizeof(pac.client);
+                    recvfrom(pac.sockfd, pac.message,sizeof(pac.message) - 1, 0,(struct sockaddr *) &pac.client,&len);
+                    int  number = (int)pac.message[1];
+                    switch (number)
+                    {
+                        case 1:
+                            strcpy(namOfSrc, &pac.message[2]);
+                            if(strstr(namOfSrc, "../") != NULL)
+                            {
+                            sendErr(1, pac);
+                            }
+                            if(pac.message[strlen(namOfSrc) + 3] == 'o')
+                            {
+                            pac.options[1] = 'b';
+                            }
+                            sendPac(1, namOfSrc, pac);
+                            break;
+                        case 2: //Put
+                          // Send ERROR pac
+                            sendErr(2,pac);
+                            break;
+                        case 3://Data
+                            // Send Erro pac
+                            sendErr(2,pac);
+                            break;
+                        case 4: //Ack
+                            sendPac(next(pac.message), namOfSrc, pac);
+                            break;
+                        case 5://Error
+                            sendErr(2,pac); // þurfum að ákveða hvaða error við viljum
+                            // nokkuð viss um að við eigum ekki að gera neitt.
+                            break;
+                        default: 
+                         sendErr(4,pac);
+                        break;// client fault
+
+                    }
+
+                }
+                else
+                {   
+                    fprintf(stdout, "10 sec passed and no message\n");
+                    fflush(stdout);
+                }
     }
-
-
-	return 0;
 }
